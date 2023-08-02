06ea6d5f843fb652b6acef37fc6617e5f24adfc2
Irfan Dary <irfandarys@gmail.com>
2023-08-02 10:17:02 +0700

    Initial Commit

---
+++ TemanTuli/TemanTuliApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemanTuliApp.swift
+//  TemanTuli
+//
+//  Created by Irfan Dary Sujatmiko on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TemanTuliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
