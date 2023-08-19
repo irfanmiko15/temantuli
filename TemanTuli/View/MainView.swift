1fab97e855429e02c60395d8e287f928097fbc5e
NathaliaMinoque <75228926+NathaliaMinoque@users.noreply.github.com>
2023-08-19 13:35:05 +0700

    animation for mic
    i also added an app icon

--- TemanTuli/View/MainView.swift
+++ TemanTuli/View/MainView.swift
@@ -7,22 +7,31 @@
 
 import SwiftUI
 
+extension Animation {
+    func `repeat`(while expression: Bool, autoreverses: Bool = true) -> Animation {
+        if expression {
+            return self.repeatForever(autoreverses: autoreverses)
+        } else {
+            return self
+        }
+    }
+}
+
 struct MainView: View {
     @StateObject var speechRecognizer = SpeechRecognizer()
     @State private var isRecording = false
+    @State private var scale = 1.0
+    @State var isAnimating = false
     
     var body: some View {
         ZStack{
             Image("bg-camera")
-//                .zIndex(0)
             HStack{
                 Spacer()
                 VStack{
                     HStack{
                         VStack(alignment: .leading){
                             Text("Hello, i Adi")
-//                                .padding(.bottom, 1)
-//                                .zIndex(1)
                             Text("Did you mean: Hello, i am Adi")
                                 .font(.caption2)
                                 .foregroundColor(.gray)
@@ -30,21 +39,15 @@ struct MainView: View {
                         .padding()
                         .background(Color("bg-primary"))
                         .cornerRadius(8)
-                       Spacer()
+                        Spacer()
                     }
                     .padding(.vertical, 4)
                     .padding(.trailing, 36)
                     HStack{
                         Spacer()
                         VStack(alignment: .leading){
-                                        Text(speechRecognizer.transcript)
+                            Text(speechRecognizer.transcript)
                                 .foregroundColor(.black)
-                            //            .padding()
-//                            Text("Hello Adi, nice to meet you")
-//                                .padding(.bottom, 1)
-//                            Text("Did you mean: Hello, i am Adi")
-//                                .font(.caption2)
-//                                .foregroundColor(.gray)
                         }
                         .padding()
                         .background(Color("bg-primary"))
@@ -55,28 +58,48 @@ struct MainView: View {
                     
                     Spacer()
                     
-                    Button {
-                        if !isRecording {
-                            speechRecognizer.transcribe()
-                        } else {
-                            speechRecognizer.stopTranscribing()
+                    ZStack{
+                        
+                        Image("mic-circle-2")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 88)
+                            .opacity(isAnimating ? 1 : 0.0) // Apply opacity animation
+                            .animation(Animation.easeInOut(duration: 2).repeat(while: isAnimating))
+                        
+                        Image("mic-circle-1")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 76)
+                            .opacity(isAnimating ? 1 : 0.0) // Apply opacity animation
+                            .animation(Animation.easeInOut(duration: 1).repeat(while: isAnimating))
+                        
+                        
+                        Button {
+                            print(isAnimating)
+                            isAnimating.toggle()
+                            if !isRecording {
+                                speechRecognizer.transcribe()
+                            } else {
+                                speechRecognizer.stopTranscribing()
+                            }
+                        } label: {
+                            Image(systemName: "mic")
+                                .bold()
                         }
-                        isRecording.toggle()
-                    } label: {
-                        Image(systemName: "mic")
-                            .bold()
+                        .padding(24)
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .clipShape(Circle())
+                        
                     }
-                    .padding(24)
-                    .foregroundColor(.white)
-                    .background(.blue)
-                    .clipShape(Circle())
+                    
                 }
                 .frame(maxWidth: 240, maxHeight:320)
                 .padding(16)
                 .background(Color("bg-secondary"))
                 .cornerRadius(20)
             }
-//            .zIndex(1)
         }
         .ignoresSafeArea()
     }
